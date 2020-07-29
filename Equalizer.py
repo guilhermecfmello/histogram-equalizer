dc4e8813ab556d70d8c6133d073930f96ad49443
Guilherme Mello <guilhermecfmello@gmail.com>
2020-07-29 11:41:44 -0300

    Normalize done

--- Equalizer.py
+++ Equalizer.py
@@ -14,32 +14,41 @@ class Equalizer:
     
     def imgNormalizer(self):
         img = imread(self.file)
-        r = img[:,:,0]
-        g = img[:,:,1]
-        b = img[:,:,2]
+        
         n = np.shape(img)[0] * np.shape(img)[1]
 
+        normR, normG, normB = self.__countColors(img)
+        normR, normG, normB = self.__normalizeColors(normR, normG, normB, n)
+        
 
-        # print(np.shape(r))
-        # exit()
+        print(normR)
+        print(normG)
+        print(normB)
 
+        print("normRSum: " + str(np.sum(normR)))
+        print("normGSum: " + str(np.sum(normG)))
+        print("normBSum: " + str(np.sum(normB)))
+
+    def __countColors(self, img):
         normR = np.zeros(RGB_MAX + 1)
         normG = np.zeros(RGB_MAX + 1)
         normB = np.zeros(RGB_MAX + 1)
+        
+        r = img[:,:,0]
+        g = img[:,:,1]
+        b = img[:,:,2]
+
         for i in range(np.shape(img)[0]):
             for j in range(np.shape(img)[1]):
                 normR[r[i,j]] = normR[r[i,j]] + 1
                 normG[g[i,j]] = normG[g[i,j]] + 1
                 normB[b[i,j]] = normB[b[i,j]] + 1
 
-        # print(normR)
-        # print(np.shape(normR))
-        plt.hist(normG, bins = RGB_MAX)
-        plt.show()
-
-
-        # print(r)
-        # print(np.shape(r))
-        # print("Number of pixels: " + str(n))
-        # print(np.shape(r))
-
+        return normR, normG, normB
+    
+    def __normalizeColors(self, normR, normG, normB, n):
+        for i in range(RGB_MAX + 1):
+            normR[i] = normR[i] / n
+            normG[i] = normG[i] / n
+            normB[i] = normB[i] / n
+        return normR, normG, normB
